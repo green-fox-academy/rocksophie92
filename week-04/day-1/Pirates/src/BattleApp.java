b051fd2a8115fad1fae17655f200477f05ab69b4
sophie <toroksophie.92@gmail.com>
2017-12-13 17:49:28 +0100

    fix(test):small fixes on the classes, it still lacks some features and fine tuning

--- week-04/day-1/Pirates/src/BattleApp.java
+++ week-04/day-1/Pirates/src/BattleApp.java
@@ -3,9 +3,9 @@ import java.util.Random;
 public class BattleApp {
 
   public static void main(String[] args) {
-    Random r = new Random();
     Ship ship = new Ship("Black Pearl");
     Ship otherShip = new Ship("Flagship");
+
 
     ship.fillShip();
     otherShip.fillShip();
--- week-04/day-1/Pirates/src/Captain.java
+++ week-04/day-1/Pirates/src/Captain.java
@@ -1,5 +1,10 @@
 public class Captain extends Pirate {
   public Captain() {
+    super();
     isCaptain=true;
+    rumsConsumed = 3;
+
   }
+
+
 }
